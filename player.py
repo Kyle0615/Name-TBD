d18b38e3fb68368fdcd5c077a6fd3035eddf7996
Kyle0615 <168646065+Kyle0615@users.noreply.github.com>
2024-05-15 22:48:10 -0400

    Add files via upload
    I am starting to work on the rotation of the player so the sprite looks where the player's mouse is.

--- player.py
+++ player.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 from settings import *
 
 class Player:
@@ -11,7 +12,14 @@ class Player:
         self.rescale_image(self.image)
         self.image_size = self.image.get_size()
         self.speed = player_speed
+        self.hitbox_rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
+        self.center =
+        self.rect = self.hitbox_rect.copy()
 
+    def rotate_player(self):
+        self.mouse_coords = pygame.mouse.get_pos() #Gets mouse coordinates.
+        self.x_change_mouse_player_distance = (self.mouse_coords[0] - self.hitbox_rect)
+        self.x_change_mouse_player_distance = (self.mouse_coords[1] - self.hitbox_rect)
     def rescale_image(self, image):
         self.image_size = self.image.get_size()
         scale_size = (self.image_size[0] * .4, self.image_size[1] * .4)
@@ -32,8 +40,13 @@ class Player:
         if keys[pygame.K_d]: #IF D KEY:
             self.velocity_x = self.speed
 
+        if self.velocity_x != 0 and self.velocity_y != 0: #CHECKS IF PLAYER IS GOING DIAGONALLY
+            #When player moves diagonally it speeds up and instead of a velocity of 8 it has velocity of 8root2
+            self.velocity_x = self.velocity_x / math.sqrt(2) #Divide x movement and y movement by root 2 so speed is 8
+            self.velocity_y = self.velocity_y / math.sqrt(2)
+
     def move(self): #Moves player
-        self.pos  = self.pos + pygame.math.Vector2(self.velocity_x, self.velocity_y)
+        self.pos = self.pos + pygame.math.Vector2(self.velocity_x, self.velocity_y)
 
     def update(self):
         self.user_input() #Updates whenever player moves
